41013b7a28054fd25b64acc3a6b8928696fc84c4
Markimg22 <marquim.net.221200@hotmail.com>
2024-08-09 12:56:25 -0400

    feat: add config format on save

--- neovim/lua/plugins/none-ls.lua
+++ neovim/lua/plugins/none-ls.lua
@@ -1,14 +1,44 @@
 return {
     "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
     config = function()
         local null_ls = require("null-ls")
+
         null_ls.setup({
             sources = {
+                require("none-ls.diagnostics.eslint_d"),
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
+                -- adicionar aqui outros formatadores instalados com o LSP
             },
+            on_attach = function(clientBf, bufnr)
+                if clientBf.supports_method("textDocument/formatting") then
+                    -- Cria um autocmd que formata o buffer antes de salvar
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({
+                                filter = function(client)
+                                    return client.name == "null-ls"
+                                end,
+                                bufnr = bufnr,
+                            })
+                        end,
+                    })
+                end
+            end,
         })
 
-        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Formatter" })
+        -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Formatter" })
+
+        vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({
+                filter = function(client)
+                    return client.name == "null-ls"
+                end,
+            })
+        end, { desc = "Formatter" })
     end,
 }
